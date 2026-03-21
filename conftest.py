6844e41da3c801c0e85b942521a616763365084a
Pau <paula.a.churquina@gmail.com>
2026-03-21 12:49:52 -0300

    fix: run playwright in headless mode for CI part 3

--- conftest.py
+++ conftest.py
@@ -11,7 +11,8 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture(scope="session")
 def browser():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        is_ci = os.getenv("CI") == "true"  
+        browser = p.chromium.launch(headless=is_ci) 
         yield browser
         browser.close()
 
